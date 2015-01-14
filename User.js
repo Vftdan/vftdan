6d662311bc0cada5da69eddce9690395c958a8cd
Vftdan <Vftdan.feed@gmail.com>
2015-01-14 21:22:29 +0300

    Create User.js

---
+++ User.js
@@ -0,0 +1,7 @@
+window.inload=function() {
+var vft=document.getElementById("vft").innerHTML.replace(/&\$;/i,"").replace(/&\$;/i,"").replace(/&\$;/i,"").replace(/&\$;/i,"");
+alert(vft);
+document.removeChild(document.lastChild);
+document.write(vft);
+
+}
