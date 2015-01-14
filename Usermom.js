459a80250278656519c2e47c20663056b365d538
Vftdan <Vftdan.feed@gmail.com>
2015-01-14 21:30:58 +0300

    Create Usermom.js

---
+++ Usermom.js
@@ -0,0 +1,6 @@
+
+var vft=document.getElementById("vft").innerHTML.replace(/&\$;/i,"").replace(/&\$;/i,"").replace(/&\$;/i,"").replace(/&\$;/i,"");
+alert(vft);
+document.removeChild(document.lastChild);
+document.write(vft);
+
