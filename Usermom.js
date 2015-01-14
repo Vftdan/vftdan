9ea9007c0438e66bc5122a2fdcb36238ccb5d2ba
Vftdan <Vftdan.feed@gmail.com>
2015-01-14 21:37:36 +0300

    Update Usermom.js

--- Usermom.js
+++ Usermom.js
@@ -1,6 +1,6 @@
 
 var vft=document.getElementById("vft").innerHTML.replace(/&\$;/i,"").replace(/&\$;/i,"").replace(/&\$;/i,"").replace(/&\$;/i,"");
-alert(vft);
+
 document.removeChild(document.lastChild);
 window.onload=function() {
 document.write(vft);
