8b60d31b980601bf04350fd0bda78e0682d8a33d
Vftdan <Vftdan.feed@gmail.com>
2015-01-14 21:38:15 +0300

    Update User.js

--- User.js
+++ User.js
@@ -1,6 +1,5 @@
 window.onload=function() {
 var vft=document.getElementById("vft").innerHTML.replace(/&\$;/i,"").replace(/&\$;/i,"").replace(/&\$;/i,"").replace(/&\$;/i,"");
-alert(vft);
 document.removeChild(document.lastChild);
 document.write(vft);
 
