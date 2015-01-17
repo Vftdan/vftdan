e339e9793714d72d2be7fb27c65df1dd9c5f6736
Vftdan <Vftdan.feed@gmail.com>
2015-01-17 21:41:33 +0300

    Update Redir.js

--- Tools/Redir.js
+++ Tools/Redir.js
@@ -1,7 +1,8 @@
-hr=location.search.replace(/\?/i,"http://");
+function redir() {hr=location.search.replace(/\?/i,"http://");
 while(hr.match(/%/i)=="%") {
 hr=hr.replace(/%3F/i,"?");
 hr=hr.replace(/%2F/i,"/");
 hr=hr.replace(/%26/i,"&");
 }
-location.href=hr
+location.href=hr};
+setTimeout("redir()",1000)
