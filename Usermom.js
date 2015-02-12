f33491bcf78250f7a7ee87051be6d52f11ab05ca
Vftdan <Vftdan.feed@gmail.com>
2015-02-12 23:21:52 +0300

    Update Usermom.js

--- Usermom.js
+++ Usermom.js
@@ -3,5 +3,5 @@ var vft=document.getElementById("vft").innerHTML.replace(/&\$;/i,"").replace(/&\
 
 document.removeChild(document.lastChild);
 window.onload=function() {
-document.write(vft+"<"+"script>iscl=true;when_loaded(true);</script"+">");
+document.write("<"+"script>iscl=true;when_loaded(true);</script"+">"+vft);
 }
