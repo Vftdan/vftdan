1161545a34c8d7d8dd6f8d3b03cd7ac9ab134d70
Vftdan <Vftdan.feed@gmail.com>
2015-03-21 17:15:15 +0300

    Update js.js

--- Ws/js.js
+++ Ws/js.js
@@ -4,12 +4,13 @@ var vft=document.getElementById("vft").innerHTML.replace(/&\$;/i,"").replace(/&\
 document.removeChild(document.lastChild);
 window.onload=function() {
 document.write("<"+"script>iscl=true;when_loaded(true);</script"+">"+vft);
-c_$(location.search);
+c_$(location.search.match(/\?[^?]*/i)[0].match(/[^?]*/i)[0]);
 }
 function c_$($get) {
 var _stat=document.getElementsByTagName("h1")[0];
 var _tit=document.getElementsByTagName("title")[0];
 _status="200 OK";
+alert($get);
 _stat.innerHTML=_status;
 _tit.innerHTML=_status+" - vftdan";
 }
