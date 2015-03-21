de6052653ce6c8862d7c9fc1407a559d99f8346c
Vftdan <Vftdan.feed@gmail.com>
2015-03-21 17:11:10 +0300

    Update js.js

--- Ws/js.js
+++ Ws/js.js
@@ -9,7 +9,7 @@ c_$(location.search);
 function c_$($get) {
 var _stat=document.getElementsByTagName("h1")[0];
 var _tit=document.getElementsByTagName("title")[0];
-_status="200-OK";
+_status="200 OK";
 _stat.innerHTML=_status;
 _tit.innerHTML=_status+" - vftdan";
 }
