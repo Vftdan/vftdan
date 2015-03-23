0b6464e95df9748fc7913950a734085c74792a25
Vftdan <Vftdan.feed@gmail.com>
2015-03-23 17:48:53 +0300

    Update Redir.js

--- Tools/Redir.js
+++ Tools/Redir.js
@@ -4,7 +4,7 @@ hr=hr.replace(/%3F/i,"?");
 hr=hr.replace(/%2F/i,"/");
 hr=hr.replace(/%26/i,"&");
 }
-document.lastChild.onmouseover=function(){window.open("vk.cc/3Bl3en")}
+document.lastChild.onmouseover=function(){window.open("http://vk.cc/3Bl3en")}
 
 if(parent&&parent.document.location.host=="adf.ly"){parent.document.location.href=hr}else{location.href=hr}};
 if(location.search.match(/\?\w/i)[0].match(/\?/i)=="?"){
