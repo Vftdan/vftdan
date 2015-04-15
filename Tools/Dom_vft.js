00d01bb6d6af4e5f3bb1352e73fddabeadfee2e0
Vftdan <Vftdan.feed@gmail.com>
2015-04-15 20:02:44 +0300

    Create Dom_vft.js

---
+++ Tools/Dom_vft.js
@@ -0,0 +1,11 @@
+var $_Dom ={
+   D:document;
+   geId:function($_temp_Id){if($_temp_Id){return document.getElementById($_temp_Id)}};
+   geClass:function($_temp_Class){if($_temp_Class){return document.getElementsByClassName($_temp_Class)}};
+   geTag:function($_temp_Tag){if($_temp_Tag){return document.getElementsByTagName($_temp_Tag)}};
+
+
+
+
+
+}
