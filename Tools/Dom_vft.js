489af6c5cefdf87a1063c84a9eb9a0102af631fe
Vftdan <Vftdan.feed@gmail.com>
2015-04-15 20:04:20 +0300

    Update Dom_vft.js

--- Tools/Dom_vft.js
+++ Tools/Dom_vft.js
@@ -1,8 +1,9 @@
 var $_Dom ={
    D:document;
-   geId:function($_temp_Id){if($_temp_Id){return document.getElementById($_temp_Id)}};
-   geClass:function($_temp_Class){if($_temp_Class){return document.getElementsByClassName($_temp_Class)}};
-   geTag:function($_temp_Tag){if($_temp_Tag){return document.getElementsByTagName($_temp_Tag)}};
+   R:D.lastChild;
+   geId:function($_temp_Id){if($_temp_Id){return D.getElementById($_temp_Id)}};
+   geClass:function($_temp_Class){if($_temp_Class){return D.getElementsByClassName($_temp_Class)}};
+   geTag:function($_temp_Tag){if($_temp_Tag){return D.getElementsByTagName($_temp_Tag)}};
 
 
 
