215ee1849adc76ddb5eacb38591ef79b6f14dd55
Vftdan <Vftdan.feed@gmail.com>
2015-07-10 12:57:20 +0300

    Update Tmp1.js

--- Tools/Tmp1.js
+++ Tools/Tmp1.js
@@ -1,7 +1,16 @@
 var $Dom={
 ge:function(s){
-var a=s.split("&");
+var a=s.split(">");
+for(i=0;i<a.length;i++){
+var t=a[i].split("&");
+var r=[];
+var st=false;
+for(j=0;j<r.length;j++){
+if(r[j].match(/[.#=]/i)==""){
+st=true;
+
 }
-
-
+}
+}
+}
 }
